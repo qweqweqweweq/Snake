3ba6092f0dcc47612a379648e0147f6691c8e90b
emptiness <alenafilimonova98@gmail.com>
2024-11-23 18:25:09 +0500

    Созданные переменные для клиента игры. Филимонова

--- SnakeWPF/MainWindow.xaml.cs
+++ SnakeWPF/MainWindow.xaml.cs
@@ -1,7 +1,10 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,10 +23,19 @@ namespace SnakeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        public static MainWindow mainWindow;
+        public ViewModelUserSetting ViewModelUserSetting = new ViewModelUserSetting();
+        public ViewModelGame ViewModelGame = null;
+        public static IPAddress remoteIPAddress = IPAddress.Parse("127.0.0.1");
+        public static int remotePOrt = 5001;
+        public Thread tRec;
+        public UdpClient receivingUdpClient;
         public Pages.Home Home = new Pages.Home();
+        public Pages.Game Game = new Pages.Game();
         public MainWindow()
         {
             InitializeComponent();
+            mainWindow = this;
             frame.Navigate(Home);
         }
     }
