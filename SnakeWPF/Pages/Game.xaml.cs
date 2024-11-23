6c6245f179e9447516e538f8c2168d7ac512a80a
emptiness <alenafilimonova98@gmail.com>
2024-11-23 19:25:41 +0500

    Программирование страницы Game. Филимонова

--- SnakeWPF/Pages/Game.xaml.cs
+++ SnakeWPF/Pages/Game.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Common;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -20,9 +21,88 @@ namespace SnakeWPF.Pages
     /// </summary>
     public partial class Game : Page
     {
+        public int StepCadr = 0;
         public Game()
         {
             InitializeComponent();
+        }
+        public void CreateUI()
+        {
+            Dispatcher.Invoke(() =>
+            {
+                if (StepCadr == 0) StepCadr = 1;
+                else StepCadr = 0;
+                canvas.Children.Clear();
+                for (int iPoint = MainWindow.mainWindow.ViewModelGames.SnakesPlayer.Points.Count - 1; iPoint >= 0; iPoint--)
+                {
+                    Snakes.Point SnakePoint = MainWindow.mainWindow.ViewModelGames.SnakesPlayer.Points[iPoint];
+                    if (iPoint != 0)
+                    {
+                        Snakes.Point NextSnakePoint = MainWindow.mainWindow.ViewModelGames.SnakesPlayer.Points[iPoint - 1];
+                        if (SnakePoint.X > NextSnakePoint.X || SnakePoint.X < NextSnakePoint.X)
+                        {
+                            if (iPoint % 2 == 0)
+                            {
+                                if (StepCadr % 2 == 0)
+                                    SnakePoint.Y -= 1;
+                                else
+                                    SnakePoint.Y += 1;
+                            }
+                            else
+                            {
+                                if (StepCadr % 2 == 0)
+                                    SnakePoint.Y += 1;
+                                else
+                                    SnakePoint.Y -= 1;
+                            }
+                        }
+                        else if (SnakePoint.Y > NextSnakePoint.Y || SnakePoint.Y < NextSnakePoint.Y)
+                        {
+                            if (iPoint % 2 == 0)
+                            {
+                                if (StepCadr % 2 == 0)
+                                    SnakePoint.X -= 1;
+                                else
+                                    SnakePoint.X += 1;
+                            }
+                            else
+                            {
+                                if (StepCadr % 2 == 0)
+                                    SnakePoint.X += 1;
+                                else
+                                    SnakePoint.X -= 1;
+                            }
+                        }
+                    }
+
+                    Brush Color;
+                    if (iPoint == 0)
+                        Color = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 0, 127, 14));
+                    else
+                        Color = new SolidColorBrush(System.Windows.Media.Color.FromArgb(255, 0, 198, 19));
+
+                    Ellipse ellipse = new Ellipse()
+                    {
+                        Width = 20,
+                        Height = 20,
+                        Margin = new Thickness(SnakePoint.X - 10, SnakePoint.Y - 10, 0, 0),
+                        Fill = Color,
+                        Stroke = Brushes.Black
+                    };
+                    canvas.Children.Add(ellipse);
+
+                    ImageBrush myBrush = new ImageBrush();
+                    myBrush.ImageSource = new BitmapImage(new Uri("pack://application:,,,/Image/apple.png"));
+                    Ellipse points = new Ellipse()
+                    {
+                        Width = 40,
+                        Height = 40,
+                        Margin = new Thickness(MainWindow.mainWindow.ViewModelGames.Points.X - 20, MainWindow.mainWindow.ViewModelGames.Points.Y - 20, 0, 0),
+                        Fill = myBrush
+                    };
+                    canvas.Children.Add(points);
+                }
+            });
         }
     }
 }
