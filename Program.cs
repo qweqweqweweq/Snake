c5672fe8ac6a4f45f28e452286430b6a10100c21
KirTrue <eltyshevaanna2006@yandex.ru>
2024-11-23 09:10:21 +0500

    Созданный метод Send. Плешкова

--- Program.cs
+++ Program.cs
@@ -1,17 +1,48 @@
-﻿using System;
+﻿using Common;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
-using System.Linq;
+using System.Net;
+using System.Net.Sockets;
 using System.Text;
-using System.Threading.Tasks;
 
 namespace Snake_FilimonovaPleshkova
 {
     class Program
     {
-        public static string List<Leaders> Leaders = new List<Leaders>();
-        public static string List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
-        public static string List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
+        public static List<Leaders> leaders = new List<Leaders>();
+        public static List<ViewModelUserSettings> remoteIPAddress = new List<ViewModelUserSettings>();
+        public static List<ViewModelGames> viewModelGames = new List<ViewModelGames>();
         private static int localPort = 5003;
         public static int MaxSpeed = 15;
+
+        private static void Send()
+        {
+            foreach(ViewModelUserSettings User in remoteIPAddress)
+            {
+                UdpClient sender = new UdpClient();
+                IPEndPoint endPoint = new IPEndPoint(
+                    IPAddress.Parse(User.IPAddress),
+                    int.Parse(User.Port));
+                try
+                {
+                    byte[] bytes = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(viewModelGames.Find(x => x.IdSnake == User.IdSnake)));
+
+                    sender.Send(bytes, bytes.Length, endPoint);
+
+                    Console.ForegroundColor = ConsoleColor.Yellow;
+                    Console.WriteLine($"Отправил данные пользователю: {User.IPAddress}:{User.Port}");
+                }
+                catch (Exception ex)
+                {
+                    Console.ForegroundColor = ConsoleColor.Red;
+                    Console.WriteLine("Возникло исключение: " + ex.ToString() + "\n" + ex.Message);
+                }
+                finally
+                {
+                    sender.Close();
+                }
+            }
+        }
     }
 }
