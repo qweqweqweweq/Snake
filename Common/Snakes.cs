f67d231114da50e97aaa035419b81d4dc699a7a2
KirTrue <eltyshevaanna2006@yandex.ru>
2024-11-23 08:54:18 +0500

    Созданный класс змеи. Плешкова

---
+++ Common/Snakes.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Common
+{
+    public class Snakes
+    {
+        public class Point
+        {
+            public int X { get; set; }
+            public int Y { get; set; }
+
+            public Point() { }
+        }
+        public enum Direction
+        {
+            Left,
+            Right,
+            Up,
+            Down,
+            Start
+        }
+        public List<Point> Points = new List<Point>();
+        public Direction direction = Direction.Start;
+        public bool GameOver = false;
+    }
+}
